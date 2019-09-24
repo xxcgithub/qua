4b8b5bd63704b9cec66e1826820ad033117b94e2
“ <1660669610@qq.com>
2019-09-24 11:27:08 +0800

    333

---
+++ src/main/java/org/demo/test/w.java
@@ -0,0 +1,13 @@
+package org.demo.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 2019/9/24
+ * Time: 11:26
+ */
+public class w {
+    public static void main(String[] args) {
+        System.out.println("ww");
+    }
+}
